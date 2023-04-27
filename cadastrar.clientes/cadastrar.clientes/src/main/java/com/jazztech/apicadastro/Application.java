f796794d3b88b0da709894697a41bbc3bbd33f6d
Jefferson <jefferson.silva@jazztech.com.br>
2023-04-27 14:14:25 -0300

    criando imagem e Dockerfile

--- cadastrar.clientes/cadastrar.clientes/src/main/java/com/jazztech/apicadastro/Application.java
+++ cadastrar.clientes/cadastrar.clientes/src/main/java/com/jazztech/apicadastro/Application.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @SpringBootConfiguration
+
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
