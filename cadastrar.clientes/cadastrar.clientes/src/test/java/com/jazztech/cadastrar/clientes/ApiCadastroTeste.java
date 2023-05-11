6648f324ff6f31b34bbf65174ca7ab75995f06ba
Jefferson <jefferson.silva@jazztech.com.br>
2023-05-11 09:50:12 -0300

    adicionando api com nova estrutura

--- cadastrar.clientes/cadastrar.clientes/src/test/java/com/jazztech/cadastrar/clientes/ApiCadastroTeste.java
+++ cadastrar.clientes/cadastrar.clientes/src/test/java/com/jazztech/cadastrar/clientes/ApiCadastroTeste.java
@@ -1,23 +1,14 @@
 package com.jazztech.cadastrar.clientes;
 
-import static io.restassured.RestAssured.*;
-
 import com.jazztech.apicadastro.Application;
 import com.jazztech.apicadastro.applicationservice.domain.entity.Clients;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import io.restassured.http.ContentType;
-import io.restassured.RestAssured.*;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import static org.hamcrest.Matchers.equalTo;
-
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 
