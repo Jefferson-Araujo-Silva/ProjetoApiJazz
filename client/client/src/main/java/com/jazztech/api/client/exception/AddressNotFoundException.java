a19e4fa1008ac293cf67396c5667bb423b3538b0
Jefferson-Araujo-Silva <jefferson.silva@sptech.school>
2023-05-13 00:33:55 -0300

    add heroku

---
+++ client/client/src/main/java/com/jazztech/api/client/exception/AddressNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jazztech.api.client.exception;
+
+public class AddressNotFoundException extends RuntimeException {
+    public AddressNotFoundException(String message) {
+        super(message);
+    }
+
+}
