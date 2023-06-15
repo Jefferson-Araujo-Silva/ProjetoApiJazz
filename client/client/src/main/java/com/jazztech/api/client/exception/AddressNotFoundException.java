be4e3ca00c674be5e32da2e78a490b74085e4bb4
Jefferson <jefferson.silva@jazztech.com.br>
2023-06-15 14:19:41 -0300

    resolving conflict

--- client/client/src/main/java/com/jazztech/api/client/exception/AddressNotFoundException.java
+++ client/client/src/main/java/com/jazztech/api/client/exception/AddressNotFoundException.java
@@ -1,15 +1,6 @@
 package com.jazztech.api.client.exception;
-
-<<<<<<< HEAD
 public class AddressNotFoundException extends RuntimeException{
     public AddressNotFoundException(String message) {
         super(message);
     }
-=======
-public class AddressNotFoundException extends RuntimeException {
-    public AddressNotFoundException(String message) {
-        super(message);
-    }
-
->>>>>>> origin/main
 }
